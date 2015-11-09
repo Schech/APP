379add37919d4de283914e94471624f9cd4f6bd5
olivierBatier <olivierbatier@gmail.com>
2015-11-09 13:28:00 +0100

    MAj pages

---
+++ Events.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link type="text/css" rel="stylesheet" href="Accueil.css"/>
+		<link type="text/css" rel="stylesheet" href="Header.css"/>
+		<link type="text/css" rel="stylesheet" href="Footer.css"/>
+		
+		<title>Evénements</title>	
+	</head>
+	<body>
+		<?php include("Header.php");?>
+		<div id="bandeaupresevent">
+			<div id="bLeft">
+				
+			</div>
+			<div id="bright">
+			</div>
+		</div>
+		<div id="bandeauimages">
+		</div>
+		<?php include("Footer.php");?>
+	</body>
+
+</html>
