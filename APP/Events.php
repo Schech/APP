41ad28ab880775377291bddd71d31ba43636d9e9
olivierBatier <olivierbatier@gmail.com>
2015-11-27 16:22:04 +0100

    SimpleventV2
    MAj de toutes les pages

---
+++ APP/Events.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link type="text/css" rel="stylesheet" href="Events.css"/>
+		<link type="text/css" rel="stylesheet" href="Header.css"/>
+		<link type="text/css" rel="stylesheet" href="Footer.css"/>
+		
+		<title>Evénements</title>	
+	</head>
+	<body>
+		<?php include("Header.php");?>
+		<div id="bandeaupresevent">
+			<div id="bleft">
+				<div class="bandeauhaut"><div class="Eventpic" style="background-image:url(http://www.rockenseine.com/wp-content/uploads/2015/09/VP2_7163RES-1024x683.jpg)"></div></div>
+				<div class="bandeaubas"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d42028.32016165403!2d2.169811143748841!3d48.82444950573356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66dfad5c08e2b%3A0x36729832bbaf3b12!2sFestival+Rock+en+Seine!5e0!3m2!1sfr!2sfr!4v1446213297181" frameborder="0"  allowfullscreen id="map"></iframe></div>
+			</div>
+			<div id="bright">
+				<div class="bandeauhaut"><h2>Rock en Seine</h2></div>
+				<div class="bandeaumilieu"></div>
+				<div class="bandeaubas"></div>
+			</div>
+		</div>
+		<div id="bandeauimages">
+		</div>
+		<?php include("Footer.php");?>
+	</body>
+
+</html>
---
+++ APP/Header.php
@@ -0,0 +1,31 @@
+	    <div id="header">
+	        <div class="bandeauhaut">
+				<div id="headergauche"> 
+					<a href="#"><img src="https://www.dropbox.com/s/1yxbj2y807fn5eq/Logo4.png?raw=1" class="imagebandeau"/></a>
+				</div>
+				<div id="headerdroit">	
+					<div class="bandeauhaut">
+						<form method="post" action="">
+							<input type="search" name="recherche" id="recherche" placeholder="Ex:Tommorrow Land, Rock, Football..."/> 
+							<input type="submit" class="boutonrecherche" value="Recherche d'événements"/>
+						</form>
+					</div>
+					<div class="bandeaubas">
+						<a href="#"><div class="Bouton"><p>Créer un événement</p></div></a>
+					</div>
+				</div>
+			</div>
+			<div class="bandeaubas">
+				<div id="menu">
+					<a href="#"><div class="Boutonmenu"><p>Accueil</p> 
+					</div></a>
+					<a href="#"><div class="Boutonmenu"><p>Evénements</p> 
+					</div></a>
+					<a href="#"><div class="Boutonmenu"><p>Mon Compte</p>
+					</div></a>  
+					<a href="#"><div class="Boutonmenu"><p>Déconnexion</p>
+					</div></a>
+				</div>
+			</div>
+			
+	    </div>
